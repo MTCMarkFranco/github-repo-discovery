dcf6a1785c85a208778b277ab4ea0c65de4a6d4a
Mark Franco <mark.franco@microsoft.com>
2025-02-07 10:15:40 -0500

    Modification for Executing deletions

--- get-hub-repositories.py
+++ get-hub-repositories.py
@@ -1,9 +1,11 @@
 import os
 import json
 import requests
+import subprocess
 from dotenv import load_dotenv
 from collections import defaultdict
 from datetime import datetime
+from colorama import Fore, Style
 
 class RepoInfo:
     def __init__(self, owner, name, url, avatar_url,archived):
@@ -46,106 +48,27 @@ if response.status_code == 200:
             archived=repo['archived']
         )
     )
-    print("finished fetching repositories!")
+    print(Fore.BLUE + "finished fetching repositories!" + Style.RESET_ALL)
 
 else:
     print(f"Failed to fetch repositories [error]: {response.status_code}")
 
+# Create the directory if it doesn't exist
+backup_dir = 'f:/projects/mtc_Backup_repos'
+if not os.path.exists(backup_dir):
+    os.makedirs(backup_dir)
 
-# 2. Send the data to Teams
-
-# Load the adaptive card template from a file
-with open('./templates/adaptive_card_template.json', 'r') as file:
-    card = json.load(file)
-    adaptiveCard = card
+# Change to the backup directory
+os.chdir(backup_dir)
 
 # Process search results in batches of 10 (To keep Adaptive card size below 28k)
-batch_size = 10
-for i in range(0, len(repo_info_list), batch_size):
-    batch = repo_info_list[i:i + batch_size]
-    
-    # Create a new adaptive card for each batch
-    adaptiveCard["body"][0]["rows"] = adaptiveCard["body"][0]["rows"][:1]
-    
-    # Add the data rows to the adaptive card
-    for item in batch:
-        adaptiveCard["body"][0]["rows"].append({
-            "type": "TableRow",
-            "cells": [
-                {
-                    "type": "TableCell",
-                    "items": [
-                        {
-                            "type": "Image",
-                            "url": f"{item.pic}",
-                            "size": "Small",
-                            "style": "Person"
-                        },
-                        {
-                            "type": "TextBlock",
-                            "text": f"{item.owner}",
-                            "wrap": True
-                        }
-                    ]
-                },
-                {
-                    "type": "TableCell",
-                    "items": [
-                        {
-                            "type": "TextBlock",
-                            "text": f"{item.name}",
-                            "wrap": True,
-                            "size": "small"
-                        },
-                        {
-                            "type": "TextBlock",
-                            "text": f"ARCHIVED",
-                            "wrap": True,
-                            "$when": f"{item.archived}",
-                            "color": "Attention"
-                        },
-                        {
-                            "type": "TextBlock",
-                            "text": f"Last Checked: {datetime.now().strftime("%d/%m/%d/%y")}",
-                            "wrap": True
-                            
-                        }
-                    ]
-                },
-                {
-                    "type": "TableCell",
-                    "items": [
-                        {
-                            "type": "ActionSet",
-                            "actions": [
-                                {
-                                    "type": "Action.OpenUrl",
-                                    "title": "...",
-                                    "url": f"{item.url}"
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        })
-    
-    # Send each adaptive card to Teams
-    payload = {
-        "type": "message",
-        "attachments": [
-            {
-                "contentType": "application/vnd.microsoft.card.adaptive",
-                "content": adaptiveCard,
-                "summary": "Outstanding Repositories"
-            }
-        ]
-    }
-    
-    response = requests.post(TEAMS_WEBHOOK, headers={"Content-Type": "application/json"}, data=json.dumps(payload))
-    if response.status_code == 200:
-        print(f"Finished sending message to Teams for batch starting at index {i}...")
-    else:
-        print(f"Failed to send message to Teams for batch starting at index {i}: {response.status_code} - {response.text}")
 
-print("Finished sending all messages to Teams!")
+for item in repo_info_list:
+
+    print(f"Processing repository: {item.url}")
+    # Clone the repository
+    clone_url = f"{item.url}.git"
+    subprocess.run(['git', 'clone', clone_url], cwd=backup_dir)
+    
+
+print(Fore.BLUE + "Finished sending all messages to Teams!" + Style.RESET_ALL)
